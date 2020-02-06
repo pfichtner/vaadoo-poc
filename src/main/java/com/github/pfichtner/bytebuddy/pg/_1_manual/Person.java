05631ce6800476599c1ab1ebaef3da46d7cfc426
Peter Fichtner (pfichtner) <github.07.fichtner@spamgourmet.com>
2020-02-06 08:32:54 +0100

    seperated class Validator

--- src/main/java/com/github/pfichtner/bytebuddy/pg/_1_manual/Person.java
+++ src/main/java/com/github/pfichtner/bytebuddy/pg/_1_manual/Person.java
@@ -1,12 +1,8 @@
 package com.github.pfichtner.bytebuddy.pg._1_manual;
 
+import static com.github.pfichtner.bytebuddy.pg._1_manual.Validator.validate;
 import static lombok.AccessLevel.PRIVATE;
 
-import java.util.Set;
-
-import javax.validation.ConstraintViolation;
-import javax.validation.ConstraintViolationException;
-import javax.validation.Validation;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
@@ -26,15 +22,6 @@ public class Person {
 
 	public static Person create(String name) {
 		return validate(new Person(name));
-	}
-
-	private static <T> T validate(T t) {
-		Set<ConstraintViolation<T>> validationResult = Validation.buildDefaultValidatorFactory().getValidator()
-				.validate(t);
-		if (!validationResult.isEmpty()) {
-			throw new ConstraintViolationException(validationResult);
-		}
-		return t;
 	}
 
 }
