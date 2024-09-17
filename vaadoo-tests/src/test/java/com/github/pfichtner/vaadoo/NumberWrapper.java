97e6bf353f431208420201d033b6da7ac215b8e2
Peter Fichtner (pfichtner) <pfichtner@users.noreply.github.com>
2024-09-17 22:37:58 +0200

    Short- any ByteWrappers added

--- vaadoo-tests/src/test/java/com/github/pfichtner/vaadoo/NumberWrapper.java
+++ vaadoo-tests/src/test/java/com/github/pfichtner/vaadoo/NumberWrapper.java
@@ -82,13 +82,98 @@ public abstract class NumberWrapper {
 
 	}
 
-	public static NumberWrapper numberWrapper(Class<?> clazz, Number number) {
-		if (clazz == int.class || clazz == Integer.class) {
-			return new IntWrapper((Integer) number);
-		} else if (clazz == long.class || clazz == Long.class) {
-			return new LongWrapper((Long) number);
+	private static class ShortWrapper extends NumberWrapper {
+
+		private final short number;
+
+		public ShortWrapper(short number) {
+			this.number = number;
 		}
-		throw new IllegalStateException("Unsupported type " + clazz + " (" + number + ")");
+
+		@Override
+		protected Class<?> type() {
+			return short.class;
+		}
+
+		@Override
+		protected Number value() {
+			return number;
+		}
+
+		@Override
+		public Object add(Number subtrahend) {
+			return (short) number + subtrahend.shortValue();
+		}
+
+		@Override
+		public Object sub(Number subtrahend) {
+			return (short) number - subtrahend.shortValue();
+		}
+
+		@Override
+		protected boolean isMin() {
+			return number == Short.MIN_VALUE;
+		}
+
+		@Override
+		protected boolean isMax() {
+			return number == Short.MAX_VALUE;
+		}
+
+	}
+
+	private static class ByteWrapper extends NumberWrapper {
+
+		private final byte number;
+
+		public ByteWrapper(byte number) {
+			this.number = number;
+		}
+
+		@Override
+		protected Class<?> type() {
+			return byte.class;
+		}
+
+		@Override
+		protected Number value() {
+			return (byte) number;
+		}
+
+		@Override
+		public Object add(Number subtrahend) {
+			return (byte) number + subtrahend.shortValue();
+		}
+
+		@Override
+		public Object sub(Number subtrahend) {
+			return (byte) number - subtrahend.shortValue();
+		}
+
+		@Override
+		protected boolean isMin() {
+			return number == Byte.MIN_VALUE;
+		}
+
+		@Override
+		protected boolean isMax() {
+			return number == Byte.MAX_VALUE;
+		}
+
+	}
+
+	public static NumberWrapper numberWrapper(Class<?> clazz, Object value) {
+		Number number = (Number) value;
+		if (clazz == int.class || clazz == Integer.class) {
+			return new IntWrapper(number.intValue());
+		} else if (clazz == long.class || clazz == Long.class) {
+			return new LongWrapper(number.longValue());
+		} else if (clazz == short.class || clazz == Short.class) {
+			return new ShortWrapper(number.shortValue());
+		} else if (clazz == byte.class || clazz == Byte.class) {
+			return new ByteWrapper(number.byteValue());
+		}
+		throw new IllegalStateException("Unsupported type " + clazz + " (" + value + ")");
 	}
 
 	@SuppressWarnings("rawtypes")
@@ -103,5 +188,10 @@ public abstract class NumberWrapper {
 	protected abstract boolean isMin();
 
 	protected abstract boolean isMax();
+
+	@Override
+	public String toString() {
+		return "NumberWrapper [type=" + type() + ", value=" + value() + "]";
+	}
 
 }
