052ebe2fcad4cc049494aeb1bb0117c91b0af1e1
Peter Fichtner (pfichtner) <pfichtner@users.noreply.github.com>
2024-09-17 11:18:39 +0200

    supporting for given types

--- vaadoo-tests/src/test/java/com/github/pfichtner/vaadoo/supplier/Primitives.java
+++ vaadoo-tests/src/test/java/com/github/pfichtner/vaadoo/supplier/Primitives.java
@@ -1,14 +1,27 @@
 package com.github.pfichtner.vaadoo.supplier;
 
+import static java.lang.annotation.ElementType.PARAMETER;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
 import java.util.Map;
+import java.util.Set;
 
 import net.jqwik.api.Arbitraries;
 import net.jqwik.api.Arbitrary;
 import net.jqwik.api.ArbitrarySupplier;
 import net.jqwik.api.Tuple;
 import net.jqwik.api.Tuple.Tuple2;
+import net.jqwik.api.providers.TypeUsage;
 
 public class Primitives implements ArbitrarySupplier<Tuple2<Class<?>, Object>> {
+
+	@Retention(RUNTIME)
+	@Target(PARAMETER)
+	public static @interface Types {
+		Class<?>[] value();
+	}
 
 	private static final Map<Class<?>, Arbitrary<?>> suppliers = Map.of( //
 			boolean.class, Arbitraries.of(true, false), //
@@ -22,7 +35,17 @@ public class Primitives implements ArbitrarySupplier<Tuple2<Class<?>, Object>> {
 
 	@Override
 	public Arbitrary<Tuple2<Class<?>, Object>> get() {
-		return Arbitraries.of(suppliers.keySet()).map(c -> Tuple.of(c, suppliers.get(c)));
+		return arbitrariesFor(suppliers.keySet());
 	}
 
+	@Override
+	public Arbitrary<Tuple2<Class<?>, Object>> supplyFor(TypeUsage targetType) {
+		return arbitrariesFor(targetType.findAnnotation(Types.class) //
+				.map(Types::value).map(Set::of) //
+				.orElseGet(() -> suppliers.keySet()));
+	}
+
+	private Arbitrary<Tuple2<Class<?>, Object>> arbitrariesFor(Set<Class<?>> classses) {
+		return Arbitraries.of(classses).flatMap(c -> suppliers.get(c).map(t -> Tuple.of(c, t)));
+	}
 }
