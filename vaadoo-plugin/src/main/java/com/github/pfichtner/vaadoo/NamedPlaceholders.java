c68a3abab69b5a39e6a5bf755d5170c008604c87
Peter Fichtner (pfichtner) <pfichtner@users.noreply.github.com>
2024-09-20 13:53:45 +0200

    house keeping

--- vaadoo-plugin/src/main/java/com/github/pfichtner/vaadoo/NamedPlaceholders.java
+++ vaadoo-plugin/src/main/java/com/github/pfichtner/vaadoo/NamedPlaceholders.java
@@ -5,6 +5,7 @@ import static java.util.regex.Matcher.quoteReplacement;
 import static java.util.regex.Pattern.compile;
 
 import java.util.Map;
+import java.util.Objects;
 import java.util.function.Function;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -15,22 +16,13 @@ public final class NamedPlaceholders {
 		super();
 	}
 
-	public static String quote(String string) {
-		return format("{%s}", string);
-	}
-
-	public static String unquote(String string) {
-		Pattern pattern = compile("\\{([^}]+)\\}");
-		Matcher matcher = pattern.matcher(string);
-		return matcher.find() ? matcher.group(1) : string;
-	}
 
 	public static String replace(String template, Map<String, Object> replacements) {
 		return replace(template, k -> replacements.getOrDefault(k, k));
 	}
 
-	public static String replace(String template, Function<String, Object> replacements) {
-		Pattern pattern = compile("(\\{[^}]+\\})");
+	public static String replace(String template, Function<String, Object> resolver) {
+		Pattern pattern = compile("\\{([^}]+)\\}");
 		String result = template;
 		while (true) {
 			Matcher matcher = pattern.matcher(result);
@@ -38,8 +30,8 @@ public final class NamedPlaceholders {
 			boolean replaced = false;
 			while (matcher.find()) {
 				String key = matcher.group(1);
-				String replacement = replacements.apply(key).toString();
-				if (!replacement.equals(key)) {
+				String replacement = resolver.apply(key).toString();
+				if (!Objects.equals(replacement, key)) {
 					replaced = true;
 					matcher.appendReplacement(sb, quoteReplacement(replacement));
 				}
