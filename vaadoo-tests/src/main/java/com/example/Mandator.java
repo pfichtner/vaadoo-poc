9cefe76308e6b2812279b5bc5f9b9b597e913fc7
Peter Fichtner (pfichtner) <pfichtner@users.noreply.github.com>
2024-09-22 23:13:04 +0200

    house keeping

--- vaadoo-tests/src/main/java/com/example/Mandator.java
+++ vaadoo-tests/src/main/java/com/example/Mandator.java
@@ -2,6 +2,7 @@ package com.example;
 
 import static java.lang.Integer.parseInt;
 import static java.lang.String.format;
+import static lombok.AccessLevel.PRIVATE;
 
 import jakarta.validation.constraints.AssertFalse;
 import jakarta.validation.constraints.AssertTrue;
@@ -9,6 +10,7 @@ import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.Pattern;
+import lombok.Getter;
 import lombok.Value;
 
 @Value
@@ -16,11 +18,12 @@ public class Mandator {
 
 	int id;
 
-	String stringValue;
+	@Getter(value = PRIVATE)
+	String cachedStringValue;
 
 	private Mandator(@Min(1) @Max(9999) int id) {
 		this.id = id;
-		this.stringValue = format("%1$4s", id).replace(' ', '0');
+		this.cachedStringValue = "Mandator " + format("%1$4s", id).replace(' ', '0');
 	}
 
 	private Mandator(@AssertTrue boolean foo,
@@ -39,7 +42,7 @@ public class Mandator {
 
 	@Override
 	public String toString() {
-		return "Mandator " + stringValue;
+		return cachedStringValue;
 	}
 
 }
