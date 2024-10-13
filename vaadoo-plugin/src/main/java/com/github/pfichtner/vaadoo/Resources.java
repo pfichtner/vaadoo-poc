1252c7e0d58f3ba2f39f1226d2ddce0675bcc11c
Peter Fichtner (pfichtner) <pfichtner@users.noreply.github.com>
2024-10-13 15:37:12 +0200

    introducing MergingResourceBundleControl

--- vaadoo-plugin/src/main/java/com/github/pfichtner/vaadoo/Resources.java
+++ vaadoo-plugin/src/main/java/com/github/pfichtner/vaadoo/Resources.java
@@ -1,11 +1,56 @@
 package com.github.pfichtner.vaadoo;
 
+import static java.util.Collections.enumeration;
 import static java.util.stream.Collectors.toMap;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.util.Enumeration;
+import java.util.Locale;
 import java.util.Map;
+import java.util.Properties;
 import java.util.ResourceBundle;
 
 public final class Resources {
+
+	private static class MergingResourceBundleControl extends ResourceBundle.Control {
+
+		@Override
+		public ResourceBundle newBundle(String baseName, Locale locale, String format, ClassLoader loader,
+				boolean reload) throws IllegalAccessException, InstantiationException, IOException {
+
+			Enumeration<URL> resources = loader
+					.getResources(toResourceName(toBundleName(baseName, locale), "properties"));
+
+			Properties mergedProps = new Properties();
+			while (resources.hasMoreElements()) {
+				try (InputStream stream = resources.nextElement().openStream()) {
+					Properties props = new Properties();
+					props.load(stream);
+					mergedProps.putAll(props);
+				}
+			}
+
+			return new ResourceBundle() {
+
+				@Override
+				protected Object handleGetObject(String key) {
+					return mergedProps.get(key);
+				}
+
+				@Override
+				public Enumeration<String> getKeys() {
+					return enumeration(mergedProps.stringPropertyNames());
+				}
+			};
+		}
+
+		@Override
+		public Locale getFallbackLocale(String baseName, Locale locale) {
+			return null;
+		}
+	}
 
 	private static final Map<String, String> messages = loadMessages();
 
@@ -18,7 +63,8 @@ public final class Resources {
 	}
 
 	private static Map<String, String> loadMessages() {
-		ResourceBundle bundle = ResourceBundle.getBundle("com/github/pfichtner/vaadoo");
+		ResourceBundle bundle = ResourceBundle.getBundle("com/github/pfichtner/vaadoo",
+				new MergingResourceBundleControl());
 		return bundle.keySet().stream().collect(toMap(key -> key, bundle::getString));
 	}
 
